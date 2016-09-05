3abd8ce6c93d8ab72c30052f0653fa48f05a2a08
Palmerig <palmeriguillermo@hotmail.com>
2016-09-05 00:27:04 -0300

    Revert "Calculadora en c"
    This reverts commit 39c5d4d7693a3eff9fcb052912462bff32a97643.

--- Tp Guille calculadora/doxygen/html/funciones_8c.js
+++
@@ -1,9 +0,0 @@
-var funciones_8c =
-[
-    [ "division", "funciones_8c.html#a47aabf7df0db1dfae2f033a5c44c08fe", null ],
-    [ "factorial", "funciones_8c.html#a923b7f84b6bd05d372a083ef8a0171a5", null ],
-    [ "ingreso", "funciones_8c.html#ac9d559f368a1d518d5ce45a7e1bae1a8", null ],
-    [ "mult", "funciones_8c.html#a71b009e4bb7670810644b886450fab8b", null ],
-    [ "restar", "funciones_8c.html#a7b610882127c59378011ddedc1a6c1da", null ],
-    [ "sumar", "funciones_8c.html#a13b53bf9ea170170c316064f7924035a", null ]
-];
--- Tp Guille calculadora/doxygen/html/search/all_72.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['restar',['restar',['../funciones_8c.html#a7b610882127c59378011ddedc1a6c1da',1,'restar(float x, float y):&#160;funciones.c'],['../funciones_8h.html#a75b986b25a17ed5cf2a3d5e6dd4d426d',1,'restar(float, float):&#160;funciones.c']]]
-];
